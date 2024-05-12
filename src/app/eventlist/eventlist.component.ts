a829635b810561093d1e7b5b25edc83cd4fe75ae
vishwasFullStack <vishu1kadam@gmail.com>
2024-05-12 10:36:28 +0530

    push base code

---
+++ src/app/eventlist/eventlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-eventlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './eventlist.component.html',
+  styleUrl: './eventlist.component.scss'
+})
+export class EventlistComponent {
+
+}
---
+++ src/app/startbreadcrumb/startbreadcrumb.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-startbreadcrumb',
+  standalone: true,
+  imports: [],
+  templateUrl: './startbreadcrumb.component.html',
+  styleUrl: './startbreadcrumb.component.scss'
+})
+export class StartbreadcrumbComponent {
+
+}
