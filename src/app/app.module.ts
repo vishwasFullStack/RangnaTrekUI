34ff7464ba48dec27d2f917ab16c947475ef63c9
vishwasFullStack <vishu1kadam@gmail.com>
2024-05-28 06:11:55 +0530

    commit floder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MenubarComponent } from './menubar/menubar.component';
+import { SearchComponent } from './search/search.component';
+import { FooterComponent } from './footer/footer.component';
+import { EventlistComponent } from './eventlist/eventlist.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HeaderComponent,
+    SearchComponent,
+    MenubarComponent,    
+    EventlistComponent,
+    FooterComponent,
   ],
   providers: [],
   bootstrap: [AppComponent]
